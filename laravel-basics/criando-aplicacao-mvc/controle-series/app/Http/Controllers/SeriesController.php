cb741fffcce06634041b2400192f3b1192a2293d
Gabriel Erick <gabrielmmve13@gmail.com>
2026-03-17 18:00:05 -0300

    convenções de nome para Controller

--- laravel-basics/criando-aplicacao-mvc/controle-series/app/Http/Controllers/SeriesController.php
+++ laravel-basics/criando-aplicacao-mvc/controle-series/app/Http/Controllers/SeriesController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
-class SeriesController
+use Illuminate\Http\Request;
+
+// todo controller que criamos é um "Controller" ou seja, eles extedem da classe "Controller", são classes filhas do mesmo e tem acesso a tudo o que ele tem (métodos, atributos, constantes....) - "Controller" é uma super classe
+class SeriesController extends Controller
 {
-    public function listarSeries() {
+    public function index(): void
+    {
         $series = [
             "Arcane",
             "Haikyuu",
@@ -13,7 +17,7 @@ class SeriesController
         $html = '<ul>';
         foreach ($series as $serie) {
             // $html .= "<li>$serie</li>" onde o operador de atribuição ".=" é em PHP uma atribuidor de atribuição de concatenação, ou seja, ele concatena o valor a direita ao final do valor a esquerda, ou seja, concatena "<li>$serie</li>" (valor a direita) com o final do valor a esquerda ($html), utilizada para juntar textos, construir strings longas ou adicionar textos ou elementos HTML 
-            $html .= "<li>$serie</li>";    
+            $html .= "<li>$serie</li>";
         }
         $html .= '</ul>';
 
