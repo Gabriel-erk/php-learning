316536dd021c968786a216689da6f07aa12c43a6
Gabriel Erick <gabrielmmve13@gmail.com>
2026-03-18 19:24:05 -0300

    aprendendo sobre Request x Response em Laravel

--- laravel-basics/criando-aplicacao-mvc/controle-series/app/Http/Controllers/SeriesController.php
+++ laravel-basics/criando-aplicacao-mvc/controle-series/app/Http/Controllers/SeriesController.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+// classe "Request" é uma classe do Laravel que representa uma requisição HTTP feita pelo cliente, ela encapsula todas as informações relacionadas a essa requisição, como os dados enviados, os parâmetros da URL, os cabeçalhos, entre outros. Ela é usada para acessar e manipular os dados da requisição de forma fácil e organizada dentro dos controladores do Laravel.
+// classe já é importada automaticamente ao gerarmos um controller via linha de comando (terminal), pois o controller em si (como descrito no Notion) recebe uma requisição (Request) e retorna uma resposta (Response), então sempre que falamos de Http no geral, esse é o comportamento esperado de um controller (tanto que os controllers (ou controladores em portugues) no laravel ficam dentro da pasta Http, para ilustrar isso melhor)
 use Illuminate\Http\Request;
 
 // todo controller que criamos é um "Controller" ou seja, eles extedem da classe "Controller", são classes filhas do mesmo e tem acesso a tudo o que ele tem (métodos, atributos, constantes....) - "Controller" é uma super classe
 class SeriesController extends Controller
 {
-    public function index(): void
+    // logo, como descrito acima, aqui estaremos recebdno uma requisição por parâmetro, que nos permite obter várias informações da requisição, como: URL, detalhes da queryString, input que viria de um formulário e etc, logo, também podemos retornar uma resposta (Response)
+    public function index(Request $request): string
     {
         $series = [
             "Arcane",
@@ -21,6 +23,6 @@ class SeriesController extends Controller
         }
         $html .= '</ul>';
 
-        echo $html;
+        return $html;
     }
 }
