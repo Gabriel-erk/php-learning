17df83efdbfae2380f0ea044995cbf6eae0a9455
Gabriel Erick <gabrielmmve13@gmail.com>
2026-03-22 13:33:10 -0300

    estrutura inicial do exercicio de crud de series, definindo view home, instalando dependencia (bootstrap), model, migration, controller e rota

---
+++ laravel-basics/criando-aplicacao-mvc/exercios-criando-aplicacao-mvc/crud-series/app/Models/Serie.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+// Eloquent ORM vai procurar a tabela "series", plural do nosso model aqui == Serie, então não precisamode um protected $table = 'series' por ex, nesse caso, apenas essa estrutura aqui é o suficiente
+class Serie extends Model
+{
+    //
+}
