163594307cc0f981ad228d1078f698a17db76e58
Gabriel Erick <gabrielmmve13@gmail.com>
2026-03-31 21:29:46 -0300

    structuring exercise about oop (1)

---
+++ oop/desafios-use-interface-namespaces-traits-e-excecoes/to-fix/src/Entities/ClienteTd.php
@@ -0,0 +1,10 @@
+<?php
+
+class ClienteTd
+{
+   static int $id = -1;
+   public function __construct(readonly public string $nome, readonly public string $email)
+   {
+      $this->id += 1;
+   }
+}
---
+++ oop/desafios-use-interface-namespaces-traits-e-excecoes/to-fix/src/Entities/ProdutoTd.php
@@ -0,0 +1,10 @@
+<?php 
+
+class ProdutoTd 
+{
+    static int $id = -1;
+    // regra básica: preço do produto precisa ser maior que 0
+    public function __construct(readonly public string $nome, readonly public float $preco){
+        $this->id += 1;
+    }
+}
