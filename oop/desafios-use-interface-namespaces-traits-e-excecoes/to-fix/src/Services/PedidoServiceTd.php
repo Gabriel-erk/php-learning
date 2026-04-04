b679189473d1ab407c1c4d0374d2d6a0f40feaf8
Gabriel Erick <gabrielmmve13@gmail.com>
2026-04-03 23:46:42 -0300

    pasta e estrutura dos services

---
+++ oop/desafios-use-interface-namespaces-traits-e-excecoes/to-fix/src/Services/PedidoServiceTd.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace Fix\Services;
+use Fix\Entities\PedidoTd;
+
+class PedidoServiceTd 
+{
+    // cria um novo objeto do tipo pedido, podendo ser "fisico" ou "digital"
+    public function criarPedido(){
+
+    }
+
+    // adiciona produto ao pedido
+    public function adicionarProduto(){
+
+    }
+
+    public function processarPedido(){
+
+    }
+
+    public function cancelarPedido(){
+
+    }
+
+    public function listarPedidos(){
+
+    }
+
+    public function calculoFaturamentoToal(){
+        
+    }
+}
