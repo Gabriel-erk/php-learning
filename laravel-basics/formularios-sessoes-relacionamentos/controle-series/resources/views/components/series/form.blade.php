79203b9687168d9bf6f1e33c3912ed3ce55823f5
Gabriel Erick <gabrielmmve13@gmail.com>
2026-05-06 16:47:08 -0300

    process of validation and showind errors

--- laravel-basics/formularios-sessoes-relacionamentos/controle-series/resources/views/components/series/form.blade.php
+++ laravel-basics/formularios-sessoes-relacionamentos/controle-series/resources/views/components/series/form.blade.php
@@ -1,3 +1,15 @@
+{{-- SE houver ALGUM ou QUALQUER erro/dado, ele entra no if e exibe todos os erros pra mim --}}
+{{--  --}}
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            {{-- $errors é UM OBJETO (MessageBag), ou seja, nao posso trata-lo como uma lista como eu estava fazendo antes ($error as $error), pois ele nao é uma lista para eu tratá-lo assim, logo, preciso pegar TODOS os erros em forma de lista, onde o método ->all() faz isso por nós, assim permitindo que eu a trate como uma lista finalmente  --}}
+            @foreach ($errors->all() as $error)    
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 <form action="{{ $action }}" method="post">
     @csrf
     {{-- se o campo nome existe, é pq é uma atualização, e caso seja uma atualização, ele insere o método PUT --}}
