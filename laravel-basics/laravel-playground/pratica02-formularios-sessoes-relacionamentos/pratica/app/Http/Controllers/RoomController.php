a5bb8cea01753ed70366687160c2c0dfdb163014
Gabriel Erick <gabrielmmve13@gmail.com>
2026-05-25 18:02:22 -0300

    criando requests de 'Room' + desenvolvimento de CRUD de 'Room'

--- laravel-basics/laravel-playground/pratica02-formularios-sessoes-relacionamentos/pratica/app/Http/Controllers/RoomController.php
+++ laravel-basics/laravel-playground/pratica02-formularios-sessoes-relacionamentos/pratica/app/Http/Controllers/RoomController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Room\RoomStoreRequest;
+use App\Models\Room;
+use Exception;
 use Illuminate\Http\Request;
 
 class RoomController extends Controller
@@ -11,7 +14,15 @@ class RoomController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            $rooms = Room::all();
+
+            return view('rooms.index', compact('rooms'))->with('message.status');
+        } catch (\Throwable $th) {
+            $errorMessage = $th->getMessage();
+
+            return view('rooms.index')->with('message.status', "Não foi possível recuperar as salas: '{$errorMessage}'");
+        }
     }
 
     /**
@@ -19,31 +30,42 @@ class RoomController extends Controller
      */
     public function create()
     {
-        //
+        return view('rooms.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(RoomStoreRequest $request)
     {
-        //
+        try {
+            $room = Room::create()->fill($request->all());
+            $room->save();
+
+            return to_route('rooms.index')->with('message.status', "Quarto: '{$room->name}' criado com sucesso!");
+        } catch (\Throwable $th) {
+            $errorMessage = $th->getMessage();
+
+            return to_route('rooms.index')->with('message.status', "Não foi possível criar o quarto: '{$errorMessage}'");
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    // usando Route Model Blinding, o laravel vai executar pro debaixo dos panos: Rooms::findOrFail(valorPassadoPorParametro == valor do parâmetro $room, que será um id), logo, caso não encontre, já executará um erro do status 404 == not found
+    public function show(Room $room)
     {
-        //
+        // devido ao Route Model Blinding, ele tentará encontar um registro na tabela Rooms com o valor passado pela rota até este método aqui (valor que estará presente em $room, porém $room será convertida para um objeto do tipo Room caso o valor passado pela rota até o método show realmente existir na tabela Rooms, pois por debaixo dos panos, essa linha de parâmetro: Room $room, pega o valor recebido pela rota jogando em $room (normalmente um id) e roda: Room::findOrFail(valorPassadoPelaRotaAtéOmétodoShowVulgoMetodoAtual), caso encontre, passa esse objeto do tipo room para nosso método show atual, se não, retorna um status 404 == não encontrado, logo, não precisamos ficar tratando com try - catch, pois usando Route Model Blinding, ele dificilmente leria o nosso catch em caso de erro pois ele só entra no método show SE ENCONTRAR o objeto que procuramos com base no valor (id) que recebemos por parâmetro pela rota do tipo get)
+        return view('rooms.show', compact('room'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Room $room)
     {
-        //
+        return view('rooms.edit', compact('room'));
     }
 
     /**
@@ -57,8 +79,16 @@ class RoomController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Room $room)
     {
-        //
+        try {
+            $room->delete();
+
+            return to_route('rooms.index')->with('message.status', "Quarto: '{$room->name}' deletado com sucesso!");
+        } catch (\Throwable $th) {
+            $errorMessage = $th->getMessage();
+            
+            return to_route('rooms.index')->with('message.status', "Não foi possível excluír o quarto: '{$errorMessage}'");
+        }
     }
 }
