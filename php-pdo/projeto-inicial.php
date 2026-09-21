3198d92e6b96f3ec8c727ed30c6d5f1952623f47
Gabriel Erick <gabrielmmve13@gmail.com>
2026-09-21 19:21:16 -0300

    understanding student class and main lines of the file 'projeto-inicial.php'

--- php-pdo/projeto-inicial.php
+++ php-pdo/projeto-inicial.php
@@ -1,13 +1,17 @@
 <?php
-
+// utilizando um namesapce
 use Alura\Pdo\Domain\Model\Student;
 
+// arquivo para chamar o arquivo de autoload para nosso projeto (require_once chama apenas uma vez)
 require_once 'vendor/autoload.php';
 
+// instanciando um objeto da classe estudadnte (que trouxemos para este arquivo graças ao nosso 'use' nas primeiras linhas de código que traz a classe 'Student' para cá)
 $student = new Student(
     null,
     'Vinicius Dias',
+    // instancia de um objeto da classe DateTimeImmutable, que estamos tendo acesso por estamos chamando do namespace 'raiz' do PHP graças a barra ao contrário antes da classe (\) 
     new \DateTimeImmutable('1997-10-15')
 );
 
+// exibindo sua idade
 echo $student->age();
--- php-pdo/src/Domain/Model/Student.php
+++ php-pdo/src/Domain/Model/Student.php
@@ -4,8 +4,10 @@ namespace Alura\Pdo\Domain\Model;
 
 class Student
 {
-    private ?int $id;
+    private ?int $id; // para identificarmos o numero de registro do nosso estudante  - ? aqui significa que o valor do nosso id pode ser nullo, e int que pode ser inteiro, logo  ?int' quer dizer que o valor da propriedade $id pode ser nullo ou inteiro
     private string $name;
+    // \ aqui significa que estou chamando a interface do namespace global do php e evita que eu tenha de importar a interface\classe no começo do programa (ex: use DateTimeInterface)
+    // DateTimeInterface é uma interface do php que recebe objetos do tipo data\hora que suas classes implementam ela (DateTimeInterface)
     private \DateTimeInterface $birthDate;
 
     public function __construct(?int $id, string $name, \DateTimeInterface $birthDate)
@@ -15,21 +17,26 @@ class Student
         $this->birthDate = $birthDate;
     }
 
+    // getter da propriedad ID
     public function id(): ?int
     {
         return $this->id;
     }
 
+    // getter da propriedade name do nosso objeto instanciado a partir desta classe
     public function name(): string
     {
         return $this->name;
     }
 
+    // getter da propriedade BirthDate
+    // retorna algo que implemente a interface DateTimeInterface
     public function birthDate(): \DateTimeInterface
     {
         return $this->birthDate;
     }
 
+    // regra de negócio para calcular idade com base na porpriedade birthDate usando método de manipulação de data
     public function age(): int
     {
         return $this->birthDate
